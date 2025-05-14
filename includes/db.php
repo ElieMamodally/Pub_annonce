381aec24fc750564575e80ed6af6cab02a53edd0
ElieMamodally <148630656+ElieMamodally@users.noreply.github.com>
2025-05-14 23:58:48 +0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -9,12 +9,12 @@ $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
 
 $conn = new mysqli($host, $user, $pass, $db);
-// try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     // Journalisation des erreurs
-//     error_log("Erreur de connexion à la base de données : " . $e->getMessage());
-//     die("Une erreur est survenue. Veuillez réessayer plus tard.");
-// }
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Journalisation des erreurs
+    error_log("Erreur de connexion à la base de données : " . $e->getMessage());
+    die("Une erreur est survenue. Veuillez réessayer plus tard.");
+}
 ?>
