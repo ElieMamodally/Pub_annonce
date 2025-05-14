b22b4e0c4f33085156bf1e13e0654e304e536685
ElieMamodally <148630656+ElieMamodally@users.noreply.github.com>
2025-05-15 00:18:16 +0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,12 @@
 <?php
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$db   = getenv("DB_NAME");
-
-$conn = new mysqli($host, $user, $pass, $db);
-
+try {
+    $pdo = new PDO(
+        "mysql:host=" . getenv("DB_HOST") . ";dbname=" . getenv("DB_NAME") . ";charset=utf8",
+        getenv("DB_USER"),
+        getenv("DB_PASS")
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
 ?>
-
