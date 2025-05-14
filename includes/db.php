235933978262b4e56d07709b147e8f9d6ebbbdf3
ElieMamodally <148630656+ElieMamodally@users.noreply.github.com>
2025-05-14 23:54:32 +0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,20 @@
 <?php
-$host = "localhost";
-$dbname = "magis_pub";
-$username = "root"; // Modifier si nécessaire
-$password = "";
+// $host = "localhost";
+// $dbname = "magis_pub";
+// $username = "root"; // Modifier si nécessaire
+// $password = "";
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
 
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    // Journalisation des erreurs
-    error_log("Erreur de connexion à la base de données : " . $e->getMessage());
-    die("Une erreur est survenue. Veuillez réessayer plus tard.");
-}
+$conn = new mysqli($host, $user, $pass, $db);
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     // Journalisation des erreurs
+//     error_log("Erreur de connexion à la base de données : " . $e->getMessage());
+//     die("Une erreur est survenue. Veuillez réessayer plus tard.");
+// }
 ?>
